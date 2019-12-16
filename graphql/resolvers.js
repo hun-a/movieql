c247f7d7b579faa2bb8aa8552ca76f8a17f47085
hun-a <seunghun.chan@gmail.com>
2019-12-17 00:03:03 +0900

    Create the delete mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,4 @@
-import { getMoviews, getById, addMovie } from './db';
+import { getMoviews, getById, addMovie, deleteMovie } from './db';
 
 const resolvers = {
   Query: {
@@ -6,7 +6,8 @@ const resolvers = {
     movie: (_, { id }) => getById(id)
   },
   Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score)
+    addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovie: (_, { id }) => deleteMovie(id)
   }
 };
 
