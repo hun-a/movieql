0aa762daed47af67e4cab38309347a60f23abaab
hun-a <seunghun.chan@gmail.com>
2019-12-17 00:25:30 +0900

    Update the getMovies function

--- graphql/db.js
+++ graphql/db.js
@@ -2,9 +2,6 @@ import fetch from 'node-fetch';
 
 const API_URL = 'http://yts.am/api/v2/list_movies.json';
 
-export const getMovies = () => new Promise((resolve, reject) => {
-  fetch(`${API_URL}`)
-    .then(res => res.json())
-    .then(json => resolve(json.data.movies))
-    .catch(reject);
-});
+export const getMovies = () => fetch(`${API_URL}`)
+  .then(res => res.json())
+  .then(json => json.data.movies);
