96618dbfe640eb5535cbbf65c704d782743b162f
hun-a <seunghun.chan@gmail.com>
2019-12-18 00:57:43 +0900

    Add the suggestions

--- graphql/db.js
+++ graphql/db.js
@@ -4,6 +4,7 @@ import R from 'ramda';
 const BASE_URL = 'https://yts.lt/api/v2/';
 const LIST_MOVIES_URL = BASE_URL + 'list_movies.json';
 const MOVIE_DETAILS_URL = BASE_URL + 'movie_details.json';
+const MOVIE_SUGGESTION_URL = BASE_URL + 'movie_suggestions.json';
 
 export const getMovies = (limit, rating) =>
   axios(LIST_MOVIES_URL, { params: { limit, minimum_rating: rating }})
@@ -12,3 +13,7 @@ export const getMovies = (limit, rating) =>
 export const getMovie = id =>
   axios(MOVIE_DETAILS_URL, { params: { movie_id: id } })
     .then(R.path(['data', 'data', 'movie']));
+
+export const suggestions = id =>
+  axios(MOVIE_SUGGESTION_URL, { params: { movie_id: id } })
+    .then(R.path(['data', 'data', 'movies']));
