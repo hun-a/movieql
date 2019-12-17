b76c288fa2120de0314b72810f4077f2b675019c
hun-a <seunghun.chan@gmail.com>
2019-12-18 00:35:55 +0900

    Refactor the getMovies function

--- graphql/db.js
+++ graphql/db.js
@@ -1,16 +1,9 @@
-import fetch from 'node-fetch';
+import axios from 'axios';
+import R from 'ramda';
 
-const API_URL = 'http://yts.am/api/v2/list_movies.json';
+const BASE_URL = 'https://yts.am/api/v2/';
+const LIST_MOVIES_URL = BASE_URL + 'list_movies.json';
 
-export const getMovies = (limit, rating) => {
-  let REQUEST_URL = API_URL;
-  if (limit > 0) {
-    REQUEST_URL += `?limit=${limit}`;
-  }
-  if (rating > 0) {
-    REQUEST_URL += `&minimum_rating=${rating}`;
-  }
-  return fetch(REQUEST_URL)
-    .then(res => res.json())
-    .then(json => json.data.movies);
-}
+export const getMovies = (limit, rating) =>
+  axios(LIST_MOVIES_URL, { params: { limit, minimum_rating: rating }})
+    .then(R.path(['data', 'data', 'movies']));
