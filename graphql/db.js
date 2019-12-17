ed2eb70ae1d5d4a2279303a40430f4b6d0e8ad06
hun-a <seunghun.chan@gmail.com>
2019-12-18 00:53:40 +0900

    Add the getMovie function

--- graphql/db.js
+++ graphql/db.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 import R from 'ramda';
 
-const BASE_URL = 'https://yts.am/api/v2/';
+const BASE_URL = 'https://yts.lt/api/v2/';
 const LIST_MOVIES_URL = BASE_URL + 'list_movies.json';
+const MOVIE_DETAILS_URL = BASE_URL + 'movie_details.json';
 
 export const getMovies = (limit, rating) =>
   axios(LIST_MOVIES_URL, { params: { limit, minimum_rating: rating }})
     .then(R.path(['data', 'data', 'movies']));
+
+export const getMovie = id =>
+  axios(MOVIE_DETAILS_URL, { params: { movie_id: id } })
+    .then(R.path(['data', 'data', 'movie']));
